836f6ab066d0966a810bd759d15b43d3e0b208f5
Nick MakeAvoy <makeavoy@gmail.com>
2025-05-23 05:30:41 -0400

    refactor: [aid] add userdata stack to VM with weak references

--- src/lua.rs
+++ src/lua.rs
@@ -1,6 +1,6 @@
 use std::{borrow::BorrowMut, cell::RefCell, collections::HashMap, mem::take, ops::DerefMut};
 
-use gc_arena::{lock::RefLock, Arena, Collect, Gc, Mutation, Rootable};
+use gc_arena::{lock::RefLock, Arena, Collect, Gc, GcWeak, Mutation, Rootable};
 
 use crate::{
     code::OpCode,
@@ -313,6 +313,7 @@ pub struct VM<'gc> {
     // meta_lookup: HashMap<String, MetaMethod>,
     // string_meta: Option<Gc<Table>>,
     pub userdata_registry: UserDataRegistry<'gc>,
+    pub userdata_stack: Vec<GcWeak<'gc, UserDataWrapper>>,
 }
 
 
@@ -377,6 +378,19 @@ impl<'gc> VM<'gc> {
         // });
 
         let stack = [(); 256].map(|_| Value::default());
+        let userdata_stack = vec![];
+        let d: &GcWeak<'gc, UserDataWrapper> = userdata_stack.first().unwrap();
+        // d.upgrade(mc)
+        let res: Result<UserDataWrapper>=d.try_into();
+        if let Err(e)=d.try_into(){
+
+        }
+        // d.upgrade(mc)
+        // d.is_dropped(cc)
+        //
+        // Gc::up
+        // Gc::downgrade(this)o
+       // let b= d.is_dropped(mc) ;
         // let stack_top = Gc::new(mc,RefLock::new( stack.as_mut_ptr() as *mut Value) );
         // let stack = vec![];
         // let stack_top = stack.as_ptr() as *mut Value;
@@ -395,6 +409,7 @@ impl<'gc> VM<'gc> {
             open_upvalues: vec![],
             table_counter: RefCell::new(1),
             userdata_registry: UserDataRegistry::new(),
+            userdata_stack,
         }
     }
 
